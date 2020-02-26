288412e39edace55425b6fee1c8152ca9da7b8ac
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-02-26 15:49:05 +0200

    styles

--- src/container/StatsBoxContainer.js
+++ src/container/StatsBoxContainer.js
@@ -5,9 +5,9 @@ const StatsBoxContainer = (props) => {
     const {stats}=props
     return (
         <div className="container-layout">
-            <StatsBox num={stats.total_conversation_count} stats="luku" />
-            <StatsBox num={stats.total_user_message_count} stats="luku" />
-            <StatsBox num={stats.total_visitor_message_count} stats="luku" />
+            <StatsBox num={stats.total_conversation_count} stats="Total conversation count" />
+            <StatsBox num={stats.total_user_message_count} stats="Total user message count" />
+            <StatsBox num={stats.total_visitor_message_count} stats="total visitor message count" />
         </div>
     )
 }
