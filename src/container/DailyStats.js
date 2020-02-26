6d1455c43637516ca03a0dda1da0ddf7a06380b6
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-02-26 15:19:43 +0200

    table

--- src/container/DailyStats.js
+++ src/container/DailyStats.js
@@ -1,28 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Table, Menu, Icon, Pagination} from 'semantic-ui-react'
 
 const DailyStats = ({stats}) => {
+    const [page, setPage]=useState(1)
     console.log("daily", stats)
+
     if(stats.by_date) {
+        const itemsOnPage=4
+        const pages=stats.by_date.length%itemsOnPage===0? stats.by_date.length/itemsOnPage:Math.floor(stats.by_date.length/itemsOnPage)+1;
+        const activePages=()=> {
+            const endPage=page*itemsOnPage>=stats.by_date.length? stats.by_date.length:page*itemsOnPage
+            return stats.by_date.slice((page-1)*itemsOnPage, endPage)
+        }
+
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <td>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        {console.log("pages", activePages())}
+                        <Table.HeaderCell>
                             conversation_count
-                        </td>
-                        <td>
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
                             date.missed_chat_count
-                        </td>
-                        <td>
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
                             visitors_with_conversation_count
-                        </td>
-                        <td>date</td>
-                    </tr>
-                </thead>       
-                {stats.by_date.map(date => {
-                return <tr><td>{date.conversation_count}</td><td>{date.missed_chat_count}</td><td>{date.visitors_with_conversation_count}</td><td>{date.date} </td></tr>
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>date</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>       
+                {activePages().map(date => {
+                return  <Table.Row key={date.date}>
+                            <Table.Cell>{date.conversation_count}</Table.Cell>
+                            <Table.Cell>{date.missed_chat_count}</Table.Cell>
+                            <Table.Cell>{date.visitors_with_conversation_count}</Table.Cell>
+                            <Table.Cell>{date.date}</Table.Cell>
+                        </Table.Row>
             })}
-        </table>
+            </Table.Body>
+            <Table.Footer>
+                <Table.Row>
+                    <Table.HeaderCell colSpan={7}>
+                    <Pagination floated='right'
+                        boundaryRange={0}
+                        defaultActivePage={1}
+                        ellipsisItem={null}
+                        firstItem={null}
+                        lastItem={null}
+                        siblingRange={1}
+                        totalPages={pages}
+                        onPageChange={(e, pageInfo) => {
+                            setPage(pageInfo.activePage)}}
+                    />
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Footer>
+        </Table>
         )
         
     }
